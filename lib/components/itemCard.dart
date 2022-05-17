6b01fec4615d305eb56dc7f32d5a69a23d2b5554
hichamrox <h.echarifchefchaouni@ecole-ipssi.net>
2022-05-18 00:44:36 +0200

    item card

--- lib/components/itemCard.dart
+++ lib/components/itemCard.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ItemCard extends StatelessWidget {
-  const ItemCard({Key? key}) : super(key: key);
+  final double height;
+  final double width;
+  final String img;
+  final String name;
+  final String prix;
+  final VoidCallback onTapPanier;
+  final VoidCallback onTapItem;
+  const ItemCard(
+      {Key? key,
+      required this.height,
+      required this.width,
+      required this.img,
+      required this.name,
+      required this.prix,
+      required this.onTapPanier,
+      required this.onTapItem})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: height,
+      width: width,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          GestureDetector(
+            onTap: onTapItem,
+            child: Stack(
+              children: [
+                Image.asset(img),
+                Positioned(
+                  child: GestureDetector(
+                    child: Image.asset("assets/images/petitPanier.png"),
+                    onTap: onTapPanier,
+                  ),
+                  bottom: 10,
+                  right: 5,
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8, top: 5),
+            child: Text(name,
+                style: GoogleFonts.nunitoSans(
+                    color: Color.fromARGB(255, 117, 116, 116),
+                    fontWeight: FontWeight.w500,
+                    fontSize: 15)),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8),
+            child: Text(prix + " €/jour",
+                style: GoogleFonts.nunitoSans(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20)),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/pages/accueil.dart
+++ lib/pages/accueil.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lookmefront/components/itemCard.dart';
 
 class AccueilPage extends StatefulWidget {
   const AccueilPage({Key? key}) : super(key: key);
@@ -10,6 +11,20 @@ class AccueilPage extends StatefulWidget {
 class _AccueilPageState extends State<AccueilPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+    return Row(children: [
+      Container(
+        height: size.height,
+      ),
+      ItemCard(
+        height: size.height * 0.3,
+        width: size.width * 0.3,
+        img: "assets/images/robeBleu.png",
+        name: "robe bleu",
+        prix: "25",
+        onTapPanier: () {},
+        onTapItem: () {},
+      ),
+    ]);
   }
 }
